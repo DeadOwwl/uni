57f846c90b42cd0cebccd2c236f41f4ab8b8b357
Alina Kragel <74296830+DeadOwwl@users.noreply.github.com>
2023-09-10 16:18:28 +0300

    Delete 4 sem/grafs py + java directory

--- 4 sem/grafs py + java/canonical_view_arcs_list.py
+++
@@ -1,9 +0,0 @@
-f = open("input.txt")
-w = open("output.txt", 'w')
-n_condition = int(f.readline())
-d = [0] * n_condition
-for line in f:
-    a, b = [int(j) for j in line.split()]
-    d[b - 1] = a
-for i in range(n_condition):
-    w.write(str(d[i]) + ' ')
